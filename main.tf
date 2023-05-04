594fd8f9a763cbb66c6103d8da0b39fb74deeee0
kishore <ykk.cse@gmail.com>
2023-05-05 03:30:10 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+variable "name" {}
+# Create an ECS cluster
+resource "aws_ecs_cluster" "example_cluster" {
+  name = "${var.name}"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "abc-kishore"
+    key    = "kk/terraform.tfstate"
+    region = "us-east-1"
+     dynamodb_table = "example-lock-table"
+    #lock_timeout_seconds = 300
+  }
+}
